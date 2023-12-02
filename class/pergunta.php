9d44282e0fbff16070a7f541255add88623568fc
Rafael Henrique Greco <31387625+rafaelhgreco@users.noreply.github.com>
2023-12-01 21:43:35 -0300

    Add files via upload

--- class/pergunta.php
+++ class/pergunta.php
@@ -1,29 +1,31 @@
-<?php 
+<?php
 class pergunta{
 
 
     public $id_pergunta;
-    public $pergunta;	
-    public $resposta;	
+    public $pergunta;
+    public $resposta;
     public $nome_solicitante;
-    public $email_solicitante;	
-    public $situacao;	
-    public $data_pergunta;	
-    public $data_resposta;	
+    public $email_solicitante;
+    public $situacao;
+    public $data_pergunta;
+    public $data_resposta;
     public $adm_id_resposta;
     public $status_pergunta;
 
 
     public function carregar()
-    {   
+    {
         $sql = "SELECT * FROM tb_perguntas WHERE id_pergunta=".$this->id_pergunta;
         include "../conexao.php";
         $resultado = $conexao->query($sql);
         $linha = $resultado->Fetch();
         $this->id_pergunta = $linha['id_pergunta'];
         $this->pergunta = $linha['pergunta'];
+        $this->adm_id_resposta = $linha['adm_id_resposta'];
         $this->resposta = $linha['resposta'];
-       
+
+
     }
 
 
@@ -42,7 +44,7 @@ class pergunta{
         $sql = "UPDATE tb_perguntas
         SET
         pergunta = '" . $this->pergunta . "',
-        resposta = '" . $this->resposta . "', 
+        resposta = '" . $this->resposta . "',
         adm_id_resposta = '" . $this->adm_id_resposta . "',
         status_pergunta = '" . $this->status_pergunta . "'
         WHERE id_pergunta = " . $this->id_pergunta;
@@ -51,14 +53,14 @@ class pergunta{
         include "../conexao.php";
         $conexao-> exec($sql);
     }
-    
+
 
     public function receber_pergunta ()
     {
         $sql = "INSERT INTO tb_perguntas (nome_solicitante,pergunta, email_solicitante) VALUES (
             ' " .$this->nome_solicitante." ' ,
             ' " .$this->pergunta." ' ,
-            ' " .$this->email_solicitante." ' 
+            ' " .$this->email_solicitante." '
         )";
         include "../conexao.php";
         $conexao-> exec($sql);
@@ -67,7 +69,7 @@ class pergunta{
 
     public function listar_pergunta_sugerida()
     {
-        
+
         $sql = "SELECT * FROM tb_perguntas WHERE status_pergunta = 'NR' ";
         include "../conexao.php";
         $resultado = $conexao->query($sql);
@@ -88,9 +90,9 @@ class pergunta{
 
     public function listar_pergunta_postada()
     {
-        $sql = "SELECT * FROM tb_perguntas p JOIN tb_adm a 
+        $sql = "SELECT * FROM tb_perguntas p JOIN tb_adm a
         ON p.adm_id_resposta= a.adm_id
-        WHERE status_pergunta = 'RES' 
+        WHERE status_pergunta = 'RES'
         ORDER BY p.id_pergunta";
         include "../conexao.php";
         $resultado = $conexao->query($sql);
@@ -101,13 +103,13 @@ class pergunta{
 
     public function anular_pergunta()
     {
-        
-        $sql = "INSERT INTO tb_perguntas_excluidas 
+
+        $sql = "INSERT INTO tb_perguntas_excluidas
             (id_pergunta, pergunta, resposta, nome_solicitante, email_solicitante, situacao, data_pergunta, data_resposta,status_pergunta)
-            SELECT 
+            SELECT
             id_pergunta, pergunta, resposta, nome_solicitante, email_solicitante, situacao, data_pergunta, data_resposta, 'EXC'
             FROM tb_perguntas
-            WHERE id_pergunta = $this->id_pergunta";    
+            WHERE id_pergunta = $this->id_pergunta";
         include "../conexao.php";
         $conexao-> exec($sql);
     }
